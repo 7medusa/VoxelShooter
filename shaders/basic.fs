64fe421e86d957f782f004f0479f910803576e74
7medusa <m3dusa7@protonmail.me>
2025-09-27 16:38:31 +0200

    fertiger modelexporter.cpp

--- shaders/basic.fs
+++ shaders/basic.fs
@@ -4,20 +4,24 @@ layout(location = 0) out vec4 main_color;
 
 in vec3 out_normal;
 in vec3 out_position;
+in vec2 out_tex_coord;
 
 uniform vec3 u_diffuse;
 uniform vec3 u_specular;
 uniform vec3 u_emissive;
 uniform float u_shininess;
+uniform sampler2D u_diffuse_map;
 
 void main() {
+    vec3 diffuseColor = texture(u_diffuse_map, out_tex_coord);
+
     vec3 view = normalize(-out_position);
     vec3 light = normalize(vec3(100.0, 100.0, 100.0));//lichtrichtung
     //vec3 light = normalize(vec3(1, 1, 1));//lichtrichtung
     vec3 normal = normalize(out_normal);
     vec3 reflection = reflect(-light, normal);
 
-    vec3 ambient = u_diffuse * 0.2;//ambient licht
+    vec3 ambient = u_diffuse * 0.2;//ambient licht vielleicht diffuse color
     vec3 diffuse = max(dot(normal, light), 0.01) * u_diffuse;//diffuse licht
     vec3 specular = pow(max(dot(reflection, view), 0.01), u_shininess) * u_specular;
     main_color = vec4(ambient + diffuse + specular + u_emissive, 1.0);
